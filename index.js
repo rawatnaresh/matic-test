023118a14b9b49c5d742c64b599319315f499e67
Naresh Rawat <nareshrawat33@gmail.com>
2022-05-06 16:54:54 +0545

    install web3 plugin

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-import {POSClient, use} from '@maticnetwork/maticjs';
+import {use} from '@maticnetwork/maticjs';
 import {Web3ClientPlugin} from '@maticnetwork/maticjs-web3';
+
+// install web3 plugin
+use(Web3ClientPlugin);
 
 AppRegistry.registerComponent(appName, () => App);
