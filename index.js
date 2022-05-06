2c65cae9c29d598a0e8fad4e00f59b0498ac0f70
Naresh Rawat <nareshrawat33@gmail.com>
2022-05-06 16:45:49 +0545

    just import

--- index.js
+++ index.js
@@ -8,27 +8,5 @@ import {name as appName} from './app.json';
 
 import {POSClient, use} from '@maticnetwork/maticjs';
 import {Web3ClientPlugin} from '@maticnetwork/maticjs-web3';
-// import HDWalletProvider from '@truffle/hdwallet-provider';
-
-// install web3 plugin
-use(Web3ClientPlugin);
-
-// const posClient = new POSClient();
-// await posClient.init({
-//   network: 'testnet',
-//   version: 'mumbai',
-//   parent: {
-//     provider: new HDWalletProvider(privateKey, mainRPC),
-//     defaultConfig: {
-//       from: fromAddress,,
-//     },,
-//   },
-//   child: {
-//     provider: new HDWalletProvider(privateKey, childRPC),
-//     defaultConfig: {
-//       from: fromAddress,,
-//     },,
-//   },,
-// });
 
 AppRegistry.registerComponent(appName, () => App);
